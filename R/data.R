2a53cd8a4e3bc789303de74cae9dc04b2b5c13ca
mikeroswell <mike.roswell@gmail.com>
2022-04-19 15:02:16 -0400

    starting to add data

---
+++ R/data.R
@@ -0,0 +1,48 @@
+# document datasets exported with this package
+
+#' NY Times COVID case data for mid-Atlantic USA in first two pandemic years
+#'
+#' A dataset containing COVID cases and deaths for 6 US states and territories
+#' from 1 April 2020 to 31 March 2022.
+#'
+#' @format Data frame with 4368 observations of 11 variables:
+#'  - **date** Date, YYYY-MM-DD
+#'  - **state** Character, name of US state or territory
+#'  - **cases** Integer, reported daily COVID cases
+#'  - **cases_avg** Numeric, rolling daily average number of cases within
+#'  "state" in previous 2 weeks
+#'  - **cases_avg_per_100k** Numeric, rolling daily average case rate per 100k
+#'  residents in previous 2 weeks
+#'  - **deaths** Integer, reported daily COVID-related deaths
+#'  - **deaths_avg** Numeric, rolling daily average number of deaths within
+#'  "state" in previous 2-weeks
+#'  - **deaths_avg_per_100k** Numeric, rolling daily average death rate per 100k
+#'  residents in previous 2 weeks
+#'  - **ref_case_rate** Numeric, rolling daily average case rate per 100k
+#'  residents in reference 2-week time period
+#'  - **nel_rate** Numeric, case rate rescaled to reference and expressed in
+#'  "nels"
+#'  - **prop_rate** Numeric, case rate rescaled to reference and expressed as a
+#'  proprtion of the reference rate
+#'
+#'  @source  Data from The New York Times, based on reports from state and local
+#'  health agencies. \url{https://github.com/nytimes/covid-19-data}
+
+"nel_vid"
+
+#' USD-CAD exchange rate in first two COVID pandemic years
+#'
+#' A dataset with weekday average exchange rate between Canadian and US Dollars
+#' between 1 April 2020 and 31 March 2022.
+#'
+#' @format Data frame with 522 observations of 3 variables
+#' - **date** Date, "YYYY-MM-DD"
+#' - **CADtoUSD** Numeric, CAD per USD
+#' - **USDtoCAD** Numeric, USD per CAD
+#'
+#' @source Data from the Federal Reserve Economic Data (FRED), accessed with the
+#' R package **alfred**.
+
+ "exch"
+
+
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,30 @@
-## code to prepare `DATASET` dataset goes here
+# get some COVID data for the Nel and propDiff examples
+library(tidyverse)
+vid <- read.csv("https://github.com/nytimes/covid-19-data/raw/master/rolling-averages/us-states.csv")
+ref_date <- vid %>% filter(date =="2021-10-17") %>%  select(state, ref_case_rate = cases_avg_per_100k)
+new_vid<-vid %>% left_join(ref_date, by = "state")
+near_states <-c("Maryland", "Virginia", "Delaware", "District of Columbia", "West Virginia", "Pennsylvania")
 
-usethis::use_data(DATASET, overwrite = TRUE)
+nel_vid<-new_vid %>% mutate(nel_rate = log(cases_avg_per_100k/ref_case_rate)
+                            , prop_rate = cases_avg_per_100k/ref_case_rate
+                            , date = as.Date(date)) %>%
+  filter(state %in% near_states) %>%
+  filter(date > as.Date("2020-04-01")) %>%
+  filter(date < as.Date("2022-03-31")) %>%
+  select(-geoid)
+
+
+# USD-CAD exchange from St. Louis Fed
+# devtools::install_github("onnokleen/alfred")
+library(alfred)
+exch<-get_alfred_series(series_id ="DEXCAUS"
+                        , observation_start = "2020-04-01"
+                        , observation_end = "2022-03-31") %>%
+  select(-realtime_period) %>%
+  group_by(date) %>%
+  summarize(CADtoUSD = mean(DEXCAUS, na.rm = TRUE)
+            , USDtoCAD = 1/CADtoUSD)
+
+
+usethis::use_data(nel_vid, overwrite = TRUE)
+usethis::use_data(exch, overwrite = TRUE)
