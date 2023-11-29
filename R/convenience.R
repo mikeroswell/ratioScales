67aed2a5e57d45ff07c2f64ebe5a0344fbb66d63
mikeroswell <mike.roswell@gmail.com>
2023-11-29 16:44:58 -0500

    typos fixed

--- R/convenience.R
+++ R/convenience.R
@@ -11,12 +11,13 @@
 #' @param group_vars variable names (quoted or unquoted) to group `col_vars` by
 #'
 #'
-#' @concept convenience
 #' @return data.frame with all original data and new `rel_` columns for each
 #' rescaled variable
+#'
+#' @concept Convenience
+#'
 #' @export
 #'
-
 #'
 #' @examples
 #' test_df <- data.frame(grp = rep(1:3, each = 10)
@@ -30,7 +31,6 @@
 #' test_df %>%
 #'   rel_start(col_vars = "val", group_vars = "grp") %>%
 #'   print(n = 30)
-
 rel_start <- function(df, col_vars, group_vars){
   df %>%
     dplyr::group_by(dplyr::pick({{ group_vars }})) %>%
@@ -39,8 +39,4 @@ rel_start <- function(df, col_vars, group_vars){
                                   x/dplyr::first(x, na_rm = TRUE)
                                   }
                                 , .names = "rel_{.col}"))
-
 }
-
-
-
